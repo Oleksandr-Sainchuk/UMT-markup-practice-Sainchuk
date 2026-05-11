9882061c346a37577625056b78cf8e05568a3cb7
Oleksandr-09 <164053424+Oleksandr-09@users.noreply.github.com>
2026-05-11 13:41:31 +0300

    Add fetch products

---
+++ js/apiClient.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+export const apiClient = axios.create({
+  baseURL: apiBaseUrl,
+  timeout: 15_000,
+});
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    port: 4000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  preview: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
